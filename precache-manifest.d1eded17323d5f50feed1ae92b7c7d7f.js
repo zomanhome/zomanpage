91bef4a799661f91f5c74b7cfa9fec4219b37c17
zoman_hp <zomanhome@gmail.com>
2019-04-15 14:15:46 +0300

    Updates

---
+++ precache-manifest.d1eded17323d5f50feed1ae92b7c7d7f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "628caf08fc8bef3a772d160151aa120a",
+    "url": "/zomanpage/static/media/logo.628caf08.png"
+  },
+  {
+    "revision": "daca99fb4a38b2860c08de15b4a723cf",
+    "url": "/zomanpage/static/media/home.daca99fb.png"
+  },
+  {
+    "revision": "e9a8fc3e08b9b37eba11",
+    "url": "/zomanpage/static/js/runtime~main.b1fdb869.js"
+  },
+  {
+    "revision": "2aa5cabcd03c6409087d",
+    "url": "/zomanpage/static/js/main.27aa54d1.chunk.js"
+  },
+  {
+    "revision": "030a8cf7bce7b537cfde",
+    "url": "/zomanpage/static/js/2.5bb36330.chunk.js"
+  },
+  {
+    "revision": "2aa5cabcd03c6409087d",
+    "url": "/zomanpage/static/css/main.99c636e6.chunk.css"
+  },
+  {
+    "revision": "52533bdfd82798a2ea6e926b2ca3ba2b",
+    "url": "/zomanpage/index.html"
+  }
+];
